ebe9fc5d612193be690fe891ea81888bee18ba9b
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2024-10-28 10:24:40 +0100

    Created a class that can publish all weather until X date

---
+++ DynamicFlightStorageSimulation/WeatherInjector.cs
@@ -0,0 +1,68 @@
+using DynamicFlightStorageDTOs;
+
+namespace DynamicFlightStorageSimulation;
+
+public class WeatherInjector
+{
+    private readonly SimulationEventBus _eventBus;
+    private List<Weather> _weatherList;
+    private int _counter;
+    public WeatherInjector(SimulationEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    /*
+     * Accepts JSON data as strings - NOT file paths
+     */
+    public void AddWeather(params string[] jsonStrings)
+    {
+        foreach (var jsonString in jsonStrings)
+        {
+            _weatherList.AddRange(WeatherCreator.ReadWeatherJson(jsonString));
+        }
+
+        _weatherList = _weatherList.OrderBy(x => x.ValidTo).ToList();
+    }
+    
+    public async Task PublishWeatherUntil(DateTime date)
+    {
+        var weatherUntil = new List<Weather>();
+        while (_counter < _weatherList.Count)
+        {
+            if (_weatherList[_counter].ValidTo > date)
+            {
+                return;
+            }
+            
+            await _eventBus.PublishWeatherAsync(_weatherList[_counter]).ConfigureAwait(false);
+            _counter++;
+        }
+    }
+
+    public async Task PublishNextWeatherEvent()
+    {
+        await _eventBus.PublishWeatherAsync(_weatherList[_counter]).ConfigureAwait(false);
+        _counter++;
+    }
+    
+    public void RestartState()
+    {
+        _counter = 0;
+    }
+    
+    public async Task PublishWeatherUntilStateless(DateTime date)
+    {
+        var weatherUntil = new List<Weather>();
+        foreach (var weather in _weatherList)
+        {
+            if (weather.ValidTo > date)
+            {
+                return;
+            }
+
+            await _eventBus.PublishWeatherAsync(weather).ConfigureAwait(false);
+        }
+    }
+    
+}
