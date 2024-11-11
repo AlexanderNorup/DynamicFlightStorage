4279a735401acb70c73da52bd06de491afe57082
Alexander Nørup <me@alexandernorup.com>
2024-11-11 12:30:30 +0100

    Now showing errors and not crashing a web-instance if orchestrator throws

---
+++ DynamicFlightStorageUI/RunAndIgnore.cs
@@ -0,0 +1,17 @@
+﻿namespace DynamicFlightStorageUI
+{
+    public static class RunAndIgnore
+    {
+        public async static Task IgnoringExceptions(this Task action)
+        {
+            try
+            {
+                await action;
+            }
+            catch
+            {
+                // Ignore
+            }
+        }
+    }
+}
