8c6230001adb964860ab80444f5cc20d5264fbb8
Alexander Nørup <me@alexandernorup.com>
2025-04-07 09:07:44 +0200

    Added experiment_server_setup.sh

---
+++ experiment_server_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+download_url=https://github.com/AlexanderNorup/DynamicFlightStorage/releases/download/v1/experimentrunner.tar
+username=$(whoami)
+ssh_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5h3CtfrIA6fMsjKRPdmSlVICAaqF46sBImEcPbKpMZ semon20@student.sdu.dk"
+
+# Install Software
+sudo apt update
+sudo apt install -y cmake dotnet-runtime-8.0
+
+# Download and extract the experiment runner
+mkdir -p ~/experimentrunner
+cd ~/experimentrunner
+wget $download_url
+tar -xvf experimentrunner.tar
+rm experimentrunner.tar
+
+# Add user to docker group
+sudo usermod -aG docker $username
+
+# Add SSH key to authorized keys
+echo $ssh_key >> ~/.ssh/authorized_keys
+
+# Download pre-filled config file from trusted server (will prompt for auth)
+scp alex@hosting.alexandernorup.com:~/dynamicflightstorage/appsettings.json ~/experimentrunner/appsettings.json
+
+echo "Startup script complete. Please log out and back in to apply group changes."
